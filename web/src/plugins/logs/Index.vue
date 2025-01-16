fd38d8b084c634f3b91f2546e0d57637118e1098
Sai Nikhil <nikhil@openobserve.ai>
2025-01-16 18:25:56 +0530

    fix: enterprise only

--- web/src/plugins/logs/Index.vue
+++ web/src/plugins/logs/Index.vue
@@ -639,7 +639,12 @@ export default defineComponent({
         router.currentRoute.value.query.hasOwnProperty("action") &&
         router.currentRoute.value.query.action == "search_scheduler"
       ) {
-        showSearchScheduler.value = true;
+        if(config.isEnterprise == 'true'){
+          showSearchScheduler.value = true;
+        }
+        else{
+          router.back();
+        }
       }
     });
 
@@ -685,7 +690,12 @@ export default defineComponent({
           router.currentRoute.value.query.hasOwnProperty("action") &&
           router.currentRoute.value.query.action == "search_scheduler"
         ) {
-          showSearchScheduler.value = true;
+          if(config.isEnterprise == 'true'){
+            showSearchScheduler.value = true;
+          }
+          else{
+          router.back();
+        }
         }
       },
       // (action) => {
